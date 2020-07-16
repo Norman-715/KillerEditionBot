db18840df3a9bc7f5c2c0dd835f190e157135812
Norman-715 <65022312+Norman-715@users.noreply.github.com>
2020-07-16 18:24:14 +0200

    Update alive.py

--- userbot/plugins/alive.py
+++ userbot/plugins/alive.py
@@ -6,7 +6,7 @@ from platform import uname
 from userbot import ALIVE_NAME
 from userbot.utils import admin_cmd
 
-DEFAULTUSER = str(ALIVE_NAME) if ALIVE_NAME else "No name set yet nibba, check pinned in @raynaldbot"
+DEFAULTUSER = str(ALIVE_NAME) if ALIVE_NAME else "No name set yet nibba, check pinned in @lukhelebot"
 
 @command(outgoing=True, pattern="^.alive$")
 async def amireallyalive(alive):
@@ -15,8 +15,10 @@ async def amireallyalive(alive):
                       "` ☞︎︎︎ Telethon version:` **6.9.0**\n` ☞︎︎︎ Python:` **3.7.3** \n` ☞︎︎︎ by:` @KidLuk\n"
                      "` ☞︎︎︎ Bot by:`[LUKHELE](tg://user?id=794996256)\n"
                      "` ☞︎︎︎ Database Status:`**All OK!**\n"
-                     "` ☞︎︎︎ Heroku Status:`ONLINE!\n"
-                     "` ☞︎︎︎ Github Status:`ONLINE!\n"
+                     "` ☞︎︎︎ Heroku Status:`online!\n"
+                     "` ☞︎︎︎ Github Status:`online!\n"
                      "` ☞︎︎︎ Setup:` **Complete!**\n"
+                     "` ☞︎︎︎ Credits:`Aditya\n"
+                     "`            `Mr-edition\n"
                      f"` ☞︎︎︎ My peru owner`: {DEFAULTUSER}\n\n"
-                     "           [Deploy this userbot Now](https://github.com/Norman-715/LukheleBot)")
+                     "           [Deploy this userbot Now](https://github.com/Norman-715/KillerEditionBot)")
